7f779817ff73552e5eebd9862eb75d55fee1c52e
Santiago <sadominguez@fi.uba.ar>
2024-06-23 17:44:56 -0300

    emprolijo

--- js/finalizarCompra.js
+++ js/finalizarCompra.js
@@ -1,35 +1,38 @@
 function renderCartTable() {
-    // Acceder al contenido del carrito
-    const cartContent = JSON.parse(localStorage.getItem('cartContent')) || {};
-  
-    // Crear la tabla y sus columnas
-    const table = document.createElement('table');
-    table.innerHTML = `
-      <tr>
-        <th>Foto</th>
-        <th>Product ID</th>
-        <th>Precio</th>
-        <th>Precio Final</th>
-      </tr>
-    `;
-  
-    // Iterar sobre los elementos del carrito y añadirlos a la tabla
+  // Acceder al contenido del carrito
+  const cartContent = JSON.parse(localStorage.getItem('cartContent')) || {};
+
+  // Crear la tabla y sus columnas
+  const table = document.createElement('table');
+  table.innerHTML = `
+    <tr>
+      <th>Foto</th>
+      <th>Product ID</th>
+      <th>Precio</th>
+    </tr>
+  `;
+
+  // Verificar si el carrito está vacío
+  if (Object.keys(cartContent).length === 0) {
+    const container = document.querySelector('#cartTableContainer');
+    container.innerHTML = '<p class="carritoVacio">El carrito está vacío</p>'; // Mostrar mensaje
+  } else {
     Object.values(cartContent).forEach(product => {
       const row = document.createElement('tr');
       row.innerHTML = `
-        <td><img src="${product.image}" alt="Product Image" style="width: 50px; height: auto;"></td>
-        <td>${product.id}</td>
-        <td>${product.price}</td>
-        <td>${product.price * product.quantity}</td>
-      `;
+          <td><img src="${product.image}" alt="Product Image" style="width: 50px; height: auto;"></td>
+          <td>${product.id}</td>
+          <td>${product.price * product.quantity}</td>
+        `;
       table.appendChild(row);
     });
-  
+
     // Insertar la tabla en el DOM
-    const container = document.querySelector('#cartTableContainer'); // Asegúrate de tener un elemento con este ID en tu HTML
+    const container = document.querySelector('#cartTableContainer');
     container.innerHTML = ''; // Limpiar el contenedor antes de añadir la nueva tabla
     container.appendChild(table);
   }
-  
-  // Llamar a la función para renderizar la tabla
-  renderCartTable();
+}
+
+// Llamar a la función para renderizar la tabla
+renderCartTable();
